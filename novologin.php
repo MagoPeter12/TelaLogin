86d0f98efb03fa42cc66ae39ce0a91a85eda2ffd
Matheus Feitosa <matheusfeitosa.582@gmail.com>
2022-08-07 21:58:40 -0300

    Tela de login

---
+++ novologin.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <title>Login</title>
+</head>
+<body>
+<div class="principal">
+    <div class="box-left">
+        <h1>Venha fazer parte do nosso time!</h1>
+        <img src="Imagens/pata.svg" class="logo-login" alt="logo">
+    </div>
+        <div class="box-right">
+            <div class="card-login">
+                <h1>Login</h1>
+                <div class="text-field">
+                <label for="usuario">Usuario</label>
+                <input type="text" name="Usuario" placeholder="Usuario">
+                </div>
+                <div class="text-field">
+                <label for="senha">Senha</label>
+                <input type="password" name="Usuario" placeholder="Senha">
+                </div>
+                <button class="btn-login">Login</button>
+            </div>
+        </div>
+</div>
+</body>
+</html>
